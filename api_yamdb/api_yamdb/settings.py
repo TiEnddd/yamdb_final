5e5f62ef491fa8577c109cf695f1db8a4290fd86
TiEnddd <ender-first_yt@mail.ru>
2023-05-24 00:00:03 +0300

    v48

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", default='hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -66,12 +66,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", default="postgres"),
+        'USER': os.getenv("POSTGRES_USER", default="postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", default="password321Q!"),
+        'HOST': os.getenv("DB_HOST", default="db"),
+        'PORT': os.getenv("DB_PORT", default="5432")
     }
 }
 
